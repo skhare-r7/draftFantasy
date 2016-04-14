6d7995882865d8b8e0e2136617e7d94fd566b329
Shreyas Khare <shreyas@khare.ca>
2016-04-14 18:45:52 -0400

    Unpicked is now point based

--- draftGame.py
+++ draftGame.py
@@ -230,7 +230,7 @@ class draftGame:
             except:
                 pass
         if underVal is None: return 'Invalid query'
-        unpickedQuery = "select playerInfo.playerName, playerInfo.team, playerInfo.skill1, playerInfo.price from playerInfo inner join playerStatus on playerInfo.playerId = playerStatus.playerId where playerInfo.price <= ? and playerStatus.status = 'Open' order by price desc"
+        unpickedQuery = "select playerInfo.playerName, playerInfo.team, playerInfo.skill1, playerInfo.price, (select sum(points) from iplpoints where iplpoints.playerId=playerInfo.playerId) as points from playerInfo inner join playerStatus on playerInfo.playerId = playerStatus.playerId where playerInfo.price <= ? and playerStatus.status = 'Open' order by points desc"
         return self.db.sendPretty(unpickedQuery,[underVal])
     
     def processViewBans(self):
